25a5ecfc8e8898e3b96575ffd00a213205908238
baeksejin <ssjin119@gmail.com>
2024-04-02 18:03:24 +0900

    feat:오늘의 추천주류 버튼 누르면 ProductList화면으로 이동

--- src/main/jujeobview/src/App.js
+++ src/main/jujeobview/src/App.js
@@ -1,31 +1,18 @@
 import './App.css';
-import MainImg from './img/MainImg.jpg';
-import MainLogo from './img/MainLogo.png';
-import Footer from "./common/Footer";
+import {Route, Routes} from "react-router-dom";
+import Header from "./common/Header";
+import ProductList from "./product/ProductList";
+import MainPage from "./MainPage";
+
 
 function App() {
     return (
-        <div className="Main">
-            <div className="MainContainer">
-                <div className="MainHeader">
-                    <div className="MainMenu">
-                        <button>술 정보</button>
-                        <button>커뮤니티</button>
-                        <button>공지사항</button>
-                        <button>장바구니</button>
-                        <button>로그인/회원가입</button>
-                    </div>
-                </div>
-                <div className="MainContent">
-                    <div className="MainTitle">
-                        <h1>JU JEOB</h1>
-                    </div>
-                    <div className="MainBtn">
-                        <button>오늘의 추천 주류</button>
-                    </div>
-                </div>
-            </div>
-            <Footer/>
+        <div>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path='/ProductList' element={<ProductList />}>
+                </Route>
+            </Routes>
         </div>
     );
 }
--- src/main/jujeobview/src/index.js
+++ src/main/jujeobview/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './fonts.css';
+import {BrowserRouter} from "react-router-dom";
+import Footer from "./common/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+    <Footer />
+  </BrowserRouter>
 );
 
 reportWebVitals();
