48ac770b1f17df90cd8a435642aa6623c9d5ad81
baeksejin <ssjin119@gmail.com>
2024-04-03 15:16:45 +0900

    style:상품 리스트 한줄에 3개씩 보이게 수정

--- src/main/jujeobview/src/product/ProductListShow.js
+++ src/main/jujeobview/src/product/ProductListShow.js
@@ -1,10 +1,36 @@
 import './ProductList.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './ProductList.css';
 
 function ProductListShow() {
+    const [productList, setProductList] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/productList')
+            .then((response) => {
+                setProductList(response.data);
+            })
+            .catch((error) => {
+                console.error('데이터 가져오기 실패:', error);
+            });
+    }, []);
+
     return (
         <div className="ProductListShowContainer">
-            <h1>상품 정보 9개씩</h1>
+            <div className="ProductItems">
+                {productList.map((product) => (
+                    <div className="ProductItem" key={product.productNo}>
+                        <div className="ProductImg"><img src={product.img}/></div>
+                        <div className="ProductName">{product.name}</div>
+                        <div className="ProductDescription">{product.description.length > 15 ? `${product.description.substring(0, 15)}...` : product.description}</div>
+                        <div className="ProductAlchol">{product.alcohol}</div>
+                        <div className="ProductPrice">{product.price}</div>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
+
 export default ProductListShow;
