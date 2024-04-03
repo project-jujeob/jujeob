f519efe64432db4cae77a695c2b7426148ee4a54
baeksejin <ssjin119@gmail.com>
2024-04-03 13:59:18 +0900

    feat:productListDto 생성

---
+++ src/main/java/com/jujeob/dto/ProductListDto.java
@@ -0,0 +1,11 @@
+package com.jujeob.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductListDto {
+    private String name;
+    private String description;
+    private String alcohol;
+    private String price;
+}
