239dd20b42946fd8f27573023592dbca3356015d
baeksejin <ssjin119@gmail.com>
2024-04-04 17:13:58 +0900

    chore:빌드 수정, 머지오류해결

--- src/main/jujeobview/src/App.js
+++ src/main/jujeobview/src/App.js
@@ -5,12 +5,8 @@ import Login from "./member/Login";
 import Register from "./member/Register";
 import RegisterComplete from "./member/RegisterComplete";
 import MainPage from "./MainPage";
-<<<<<<< HEAD
-import ProductListShow from "./product/ProductListShow";
 import ProductItemDetail from "./product/ProductItemDetail";
-=======
 import React from "react";
->>>>>>> c3400f6a5ec6259a363c87f8af8ac670d1db45ce
 
 
 function App() {
@@ -20,7 +16,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path='/ProductList' element={<ProductList />}/>
-                {/*<Route path='/ProudctList/:productNo' component={ProductListShow} />*/}
                 <Route path='/ProuductItemDetail/:productNo' element={<ProductItemDetail /> } />
                 <Route path='/Login' element={<Login />} />
                 <Route path='/Register' element={<Register />} />
