673bf1a4925cfeaa509ff7bdd360fff58a534fa0
CJaeHo <ekrxj35@gmail.com>
2024-04-05 14:31:28 +0900

    feat: mypage 폼 구현

--- src/main/jujeobview/src/member/MyPage.js
+++ src/main/jujeobview/src/member/MyPage.js
@@ -1,6 +1,27 @@
+import './MyPage.css';
 function MyPage () {
     return (
-        <div>마이페이지</div>
+        <div>
+            <div className={"MyPage"}>
+                <div className={"MyPageForm"}>
+                    <div className={"MyPageTitleBox"}>
+                        <h1>마이페이지</h1>
+                    </div>
+                    <div className={"MyPageSideBar"}>
+                        <div><h3>주문 및 배송</h3></div>
+                        <div><h3>구매 및 예약</h3></div>
+                        <div><h3>찜 목록</h3></div>
+                        <div><h3>리뷰 내역</h3></div>
+                        <div><h3>내가 쓴 게시글</h3></div>
+                        <div><h3>회원 정보 수정</h3></div>
+                    </div>
+                    {/*<div className={"MyPageDetail"}>*/}
+                    {/*    <input>*/}
+                    {/*</div>*/}
+                </div>
+            </div>
+        </div>
+
     )
 }
 
