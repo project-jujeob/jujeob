fa61c210bee66769c07f9ba4086324c7fbf9017a
CJaeHo <ekrxj35@gmail.com>
2024-04-09 18:10:19 +0900

    feat: 회원가입, 로그인 테스트 코드 및 jwt, security 구현

--- src/main/jujeobview/src/App.js
+++ src/main/jujeobview/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import React from "react";
 import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
+import MainPage from "./MainPage";
 import ProductList from "./product/ProductList";
 import Login from "./member/Login";
+import RegisterAdult from "./member/RegisterAdult";
 import Register from "./member/Register";
 import RegisterComplete from "./member/RegisterComplete";
 import MyPage from "./member/MyPage";
-import MainPage from "./MainPage";
 import ProductItemDetail from "./product/ProductItemDetail";
-import React from "react";
 import Logout from "./member/Logout";
 // import {AuthProvider} from "./member/Context";
 
@@ -23,6 +24,7 @@ function App() {
                 <Route path='/ProductList' element={<ProductList />}/>
                 <Route path='/ProuductItemDetail/:productNo' element={<ProductItemDetail /> } />
                 <Route path='/Login' element={<Login />} />
+                <Route path='/RegisterAdult' element={<RegisterAdult />} />
                 <Route path='/Register' element={<Register />} />
                 <Route path='/RegisterComplete' element={<RegisterComplete />} />
                 <Route path='/MyPage' element={<MyPage />} />
